b5dc1a34dc6700421d949639b9fb588fe881debf
Beau Carlborg <bcarlborg@gmail.com>
2018-08-17 16:25:17 -0700

    simple udp server

--- python/udp_server.py
+++ python/udp_server.py
@@ -1,3 +1,14 @@
 from socket import *
 
-print("I am a real server")
+server_port = 12000
+server_socket = socket(AF_INET, SOCK_DGRAM)
+
+# I don't know what this function does yet? Still confusing to me?
+# what is bind used for exactly
+server_socket.bind(('', server_port))
+print("This server socket is bound and ready to go!")
+
+while true:
+    message, client_addr = server_socket.recvfrom(2048)
+    modified_message = message.upper()
+    serverSocket.sendto(modified_message, client_addr)
